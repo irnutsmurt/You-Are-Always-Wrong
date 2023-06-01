f5b9c15650b5c4fc32973045b012807335ac7d70
I R Nut Smurt <73799093+irnutsmurt@users.noreply.github.com>
2023-05-31 18:06:11 -0700

    Update youarewrong.py

--- youarewrong.py
+++ youarewrong.py
@@ -8,27 +8,28 @@ def main():
 
     # List of insult responses
     insult_responses = [
-        "Your answer is bad and you should feel bad. You must disappoint your mother so much.",
-        "Did you even try? That's not even close.",
-        "Your answer is as unimpressive as a wet firecracker.",
-        "You probably think the Earth is flat, too, don't you?",
-        "I’ve seen better answers from a Siri.",
-        "Wow, I bet you make your kindergarten teacher proud.",
-        "If you were any slower, you’d be going backward.",
-        "Do you have your helmet on? Because that was a major crash and burn.",
-        "I’ve heard smarter things from a toddler.",
-        "Your answer just lowered the room’s IQ.",
-        "I’d tell you to go think about what you’ve done, but that seems to be the problem.",
-        "Are you always this disappointing, or is today a special occasion?",
-        "I’d insult you, but I won’t do as well as nature did.",
-        "That was so terrible I think you gave me cancer!",
-        "You’re about as useful as a chocolate teapot.",
-        "Your birth certificate is an apology letter from the condom factory.",
-        "I’d give you a nasty look but you’ve already got one.",
-        "It looks like your face caught on fire and someone tried to put it out with a hammer.",
-        "I'm jealous of people who don't know you.",
-        "Your only chance of getting laid is to crawl up a chicken’s butt and wait."
+        "Wrong! Your answer is bad and you should feel bad. You must disappoint your mother so much.",
+        "Incorrect! Did you even try? That's not even close.",
+        "Nope! Your answer is as unimpressive as a wet firecracker.",
+        "You're wrong! You probably think the Earth is flat, too, don't you?",
+        "That's not right! I’ve seen better answers from a Siri.",
+        "False! Wow, I bet you make your kindergarten teacher proud.",
+        "Incorrect! If you were any slower, you’d be going backward.",
+        "Wrong answer! Do you have your helmet on? Because that was a major crash and burn.",
+        "Nope! I’ve heard smarter things from a toddler.",
+        "Incorrect! Your answer just lowered the room’s IQ.",
+        "Wrong! I’d tell you to go think about what you’ve done, but that seems to be the problem.",
+        "Nope! Are you always this disappointing, or is today a special occasion?",
+        "Incorrect! I’d insult you, but I won’t do as well as nature did.",
+        "False! That was so terrible I think you gave me cancer!",
+        "Wrong answer! You’re about as useful as a chocolate teapot.",
+        "Nope! Your birth certificate is an apology letter from the condom factory.",
+        "Incorrect! I’d give you a nasty look but you’ve already got one.",
+        "Wrong! It looks like your face caught on fire and someone tried to put it out with a hammer.",
+        "False! I'm jealous of people who don't know you.",
+        "Wrong answer! Your only chance of getting laid is to crawl up a chicken’s butt and wait."
     ]
+
 
     question = random.choice(questions)
     for character in question:
