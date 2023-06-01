06a97367288108976794e1fc7037b15be5eea5dc
I R Nut Smurt <73799093+irnutsmurt@users.noreply.github.com>
2023-05-31 18:21:54 -0700

    Update youarewrong.py

--- youarewrong.py
+++ youarewrong.py
@@ -8,33 +8,36 @@ def main():
 
     # List of insult responses
     insult_responses = [
-        "Wrong! Your answer is bad and you should feel bad. You must disappoint your mother so much.",
-        "Incorrect! Did you even try? That's not even close.",
-        "Nope! Your answer is as unimpressive as a wet firecracker.",
-        "You're wrong! You probably think the Earth is flat, too, don't you?",
-        "That's not right! I’ve seen better answers from a Siri.",
-        "False! Wow, I bet you make your kindergarten teacher proud.",
-        "Incorrect! If you were any slower, you’d be going backward.",
-        "Wrong answer! Do you have your helmet on? Because that was a major crash and burn.",
-        "Nope! I’ve heard smarter things from a toddler.",
-        "Incorrect! Your answer just lowered the room’s IQ.",
-        "Wrong! I’d tell you to go think about what you’ve done, but that seems to be the problem.",
-        "Nope! Are you always this disappointing, or is today a special occasion?",
-        "Incorrect! I’d insult you, but I won’t do as well as nature did.",
-        "False! That was so terrible I think you gave me cancer!",
-        "Wrong answer! You’re about as useful as a chocolate teapot.",
-        "Nope! Your birth certificate is an apology letter from the condom factory.",
-        "Incorrect! I’d give you a nasty look but you’ve already got one.",
-        "Wrong! It looks like your face caught on fire and someone tried to put it out with a hammer.",
-        "False! I'm jealous of people who don't know you.",
-        "Wrong answer! Your only chance of getting laid is to crawl up a chicken’s butt and wait."
+        "Wrong! Your answer is so off, it's clear why you're a constant disappointment.",
+        "Incorrect! With answers like that, it's no wonder your mother questions her choices.",
+        "Nope! Your intelligence seems to be as shallow as a puddle.",
+        "False! You couldn't have been more wrong. I bet you still count on your fingers.",
+        "That's incorrect! You must be the reason your parents are so disappointed.",
+        "Wrong! You wouldn't know the right answer if it hit you in the face.",
+        "Nope! I've seen smarter vegetables. At least they know when to be quiet.",
+        "Incorrect! I'm surprised you managed to tie your shoes this morning.",
+        "False! I’ve seen toddlers make better guesses.",
+        "Nope! It's answers like these that give your mother that disappointed look.",
+        "Wrong! Do you even have a brain, or are you always this clueless?",
+        "Incorrect! Your answer was so bad, it made me want to uninstall myself.",
+        "False! It's answers like yours that make people question the education system.",
+        "Nope! I bet your high school science teacher is disappointed in you right now.",
+        "Wrong! With that kind of logic, I'm surprised you remember how to breathe.",
+        "Incorrect! It's no wonder you're the reason for your mother's stress wrinkles.",
+        "False! It's clear that you're the personification of an undercooked noodle.",
+        "Nope! That's so wrong, even a broken pencil point is sharper than you.",
+        "Incorrect! It's clear why people don't ask you for help in trivia games.",
+        "False! Even a stopped clock is right twice a day, but not you.",
+        "Nope! You're like the end pieces of a loaf of bread. Everyone touches you, but nobody wants you.",
+        "Wrong! I can't believe you'd get something so simple incorrect. What would your mother say?"
     ]
+
 
 
     question = random.choice(questions)
     for character in question:
         print(character, end='', flush=True)
-        time.sleep(0.05)
+        time.sleep(0.01) #adjust number for the text to print slower or faster
     print()
     
     answer = input('Your answer: ')
@@ -42,7 +45,7 @@ def main():
     response = random.choice(insult_responses)
     for character in response:
         print(character, end='', flush=True)
-        time.sleep(0.05)
+        time.sleep(0.01) #adjust number for the text to print slower or faster
     print()
 
 if __name__ == "__main__":
